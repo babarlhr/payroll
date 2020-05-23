0b01e90ab6711e268812bb0dd14635cdc4d50d0d
Younis <m.younis.mahsud@gmail.com>
2020-05-23 21:06:40 +0500

    Employee extension

---
+++ employee_extension/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Employee Extension',
+    'description': 'Employee extension',
+    'author': 'ERPify Inc.',
+    'data': [
+        'views/employee.xml',
+        'security/ir.model.access.csv',
+    ],
+    'depends': ['hr'],
+    'installable': True,
+}
